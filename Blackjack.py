e10a86fdfe866e3261af678dea72c2fb15fb12d8
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2020-08-13 15:09:53 -0700

    Added bets

--- Blackjack.py
+++ Blackjack.py
@@ -2,7 +2,6 @@ import random
 
 wallet = 10
 bet = 0
-bet2 = 0
 values = {
     '2': 2,
     '3': 3,
@@ -16,67 +15,102 @@ values = {
     'J': 10,
     'Q': 10,
     'K': 10,
-    'A': 11
+    'A': 1
 }
 cards = list(values.keys())
 dealerHand = []
-playerHand = []
-playerHand2 = []
+playerHands = [[], []]
 didSplit = False
-currentHand = 1
+currentHand = 0
 
 def init():
     global wallet
     global bet
     global dealerHand
-    global playerHand
-    global playerHand2
+    global playerHands
     global didSplit
     global currentHand
 
+    bet = 0
     dealerHand.clear()
-    playerHand.clear()
-    playerHand2.clear()
+    playerHands = [[], []]
     didSplit = False
-    currentHand = 1
+    currentHand = 0
 
-    playerHand += random.choice(cards)
+    playerHands[0] += random.choice(cards)
     dealerHand += random.choice(cards)
-    playerHand += random.choice(cards)
+    playerHands[0] += random.choice(cards)
     dealerHand += random.choice(cards)
+
+    while bet <= 0:
+        print("Your current balance is {}".format(wallet))
+        response = input("Place your bet: ")
+        if response.isdigit():
+            numIn = int(response)
+            if numIn > wallet:
+                print("Your bet can't exceed your balance\n")
+            elif numIn <= 0:
+                print("You can't bet nothing\n")
+            else:
+                bet = numIn
+        else:
+            print("Please type a whole number\n")
+
+def calcHand(hand):
+    total = 0
+    aces = 0
+    for card in hand:
+        total += values[card]
+        if card == 'A':
+            aces += 1
+    for ace in range(aces):
+        if (total + 10) <= 21:
+            total += 10
+    return total
+
+def checkEnd():
+    pass
 
 def stand():
     print('stand')
     # end round
 
 def hit():
-    global playerHand
-    playerHand += random.choice(cards)
+    global playerHands
+    playerHands[currentHand] += random.choice(cards)
     pass
 
 def double():
-    global playerHand
-    playerHand += random.choice(cards)
+    global bet
+    global playerHands
+    bet *= 2
+    playerHands[currentHand] += random.choice(cards)
     # end round
 
 def split():
-    global playerHand
-    global playerHand2
+    global playerHands
     global didSplit
 
-    if not didSplit and len(playerHand) == 2 and playerHand[0] == playerHand[1]:
+    if not didSplit and len(playerHands[0]) == 2 and playerHands[0][0] == playerHands[0][1]:
         didSplit = True
-        playerHand2 += playerHand.pop()
+        playerHands[1] += playerHands[0].pop()
         
-        playerHand += random.choice(cards)
-        playerHand2 += random.choice(cards)
+        playerHands[0] += random.choice(cards)
+        playerHands[1] += random.choice(cards)
     else:
-        print("Split unavailable")
+        print("Split unavailable\n")
     pass
 
 def surrender():
-    print('surrender')
-    # end round
+    global bet
+    global playerHands
+    global didSplit
+
+    if not didSplit and len(playerHands[0]) == 2:
+        bet //= 2
+        # end round  # do not calculate win
+    else:
+        print("Surrender unavailable\n")
 
 actionDict = {
     's': stand,
@@ -94,12 +128,13 @@ actionDict = {
 
 init()
 while True:
+    print("Bet: {}".format(bet))
     print("Dealer: #, {}".format(dealerHand[1]))
     
     if didSplit:
-        print("Hand {}: {}".format(currentHand, ", ".join(playerHand if currentHand == 1 else playerHand2)))
+        print("Hand {}: {}".format(currentHand + 1, ", ".join(playerHands[currentHand])))
     else:
-        print("Player: {}".format(", ".join(playerHand)))
+        print("Player: {}".format(", ".join(playerHands[0])))
 
     print("Actions: (s)tand, (h)it, (d)ouble down, s(p)lit, s(u)rrender")
     action = input().lower()
@@ -107,4 +142,4 @@ while True:
     if action in actionDict:
         actionDict[action]()
     else:
-        print("Invalid action")
+        print("Invalid action\n")
